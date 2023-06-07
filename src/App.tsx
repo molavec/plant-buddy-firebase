f5ccee5e17abf857c685807eb022ee21723879b5
Miguel Olave <molavec@gmail.com>
2023-06-07 16:46:01 -0400

    Set Home App

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import './App.css';
 import logo from '../public/logo.png';
 
 const App = () => {
-
   return (
     <>
-      <div className="flex justify-items-center items-center h-screen w-screen  text-3xl text-rose-600">
-          <img src={logo} alt="Plant Buddy Logo" />
-          <p>Loading...</p>
+      <div 
+        className="
+          flex flex-col justify-center items-center 
+          gap-3 h-screen w-screen  text-3xl 
+          text-rose-600"
+      >
+        <img src={logo} alt="Plant Buddy Logo" />
+        <p>Loading...</p>
       </div>
     </>
   );
-}
+};
 
 export default App;
